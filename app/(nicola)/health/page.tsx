2f06f4a34bab07e20bde14cc6f8e606738a40644
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-25 01:53:44 +0600

    f

--- app/(nicola)/health/page.tsx
+++ app/(nicola)/health/page.tsx
@@ -6,10 +6,11 @@ export default function page() {
     <div className="health py-2 px-3">
     <div className="text capitalize text-4xl font-bold  sm:text-center
       sm:w-1/2 sm:mx-auto sm:py-32 py-24">
-      <div className='text-lg uppercase pb-3'>welcome to heisenberg</div>
+      <h1 className='text-lg uppercase pb-3'>welcome to heisenberg</h1>
       <div>isn't it time to care about</div>
       <div className='pb-3'>your own helth?</div>
       <div className='text-sm'>do not forget your future your lifetime depends on your health condition. if you want to live a long life then lets focus now.</div>
+
 
     </div>
    </div>
