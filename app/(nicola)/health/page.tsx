9795f7d53043901642ca77d4c1702f29beff4ccb
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-25 01:55:24 +0600

    v6

--- app/(nicola)/health/page.tsx
+++ app/(nicola)/health/page.tsx
@@ -2,16 +2,15 @@ import React from 'react'
 import Link from 'next/link'
 export default function page() {
   return (
-
     <div className="health py-2 px-3">
     <div className="text capitalize text-4xl font-bold  sm:text-center
-      sm:w-1/2 sm:mx-auto sm:py-32 py-24">
+    sm:w-1/2 sm:mx-auto sm:py-32 py-24">
       <h1 className='text-lg uppercase pb-3'>welcome to heisenberg</h1>
       <div>isn't it time to care about</div>
       <div className='pb-3'>your own helth?</div>
       <div className='text-sm'>do not forget your future your lifetime depends on your health condition. if you want to live a long life then lets focus now.</div>
 
-
+ 
     </div>
    </div>
   )
