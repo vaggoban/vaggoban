b937906d90044dceccd2c7b3da44f925a54369f0
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-28 17:20:44 +0600

    z

--- app/(nicola)/support/page.tsx
+++ app/(nicola)/support/page.tsx
@@ -13,7 +13,7 @@ export default function page() {
               <Image alt='ok' src={salim} className='w-full h-full'></Image>
             </figure>
 
-           <div className="text">
+           <div className="o0">
            <div className="line1 font-bold tracking-wider text-2xl py-3">salim bocktiar
             </div>
             
