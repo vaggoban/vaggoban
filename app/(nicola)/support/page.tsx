1792a04b0f9a6a0389a903d1b3b3d45f38de1f12
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-29 00:53:40 +0600

    its cool

--- app/(nicola)/support/page.tsx
+++ app/(nicola)/support/page.tsx
@@ -1,37 +1,33 @@
 import React from 'react'
 import Image from 'next/image'
-// import sopnil from "../img/sopnil.jpg"
+import sopnil from "../img/sopnil.jpg"
 import salim from "../img/bocktiar.jpg"
 export default function page() {
   return (
     <div>
         <div className="support grid sm:grid-cols-2 justify-center mx-auto py-4
         sm:w-3/4  sm:gap-4 capitalize">
-
         <div className="sector1 border-2 text-center">
             <figure className='w-64 border-4 mx-auto mt-5'>
               <Image alt='ok' src={salim} className='w-full h-full'></Image>
             </figure>
-
-
            <div className="line1 font-bold tracking-wider text-2xl py-3">salim bocktiar
             </div>
             <div className="line2 font-bold  text-sm">
              my name is salim bockiar . i could have been dead five years now. credit goes to HEISENBERG company.
             </div>
-
-
         </div>
-{/* 
+
           <div className="sector2 border-2 text-center">
             <figure className='w-64 border-4 mx-auto mt-5'>
               <Image alt='ok' src={sopnil} className='w-full h-full'></Image>
             </figure>
             <div className="line1 text-2xl py-3 font-bold tracking-wider">sopnil tasnim</div>
             <div className="line2 text-sm font-bold">
-              it's sopnil the president of tasnim government. my wife and kid would have been dead without the HEISENBERG. all credit goes to the heisenberg and their docktors. they doesn's do business with paitent
+              it's sopnil im the manager of tasnim company. HEISENBERG'S doctors 
+              saved my baby's life. thanks to heisenberg
             </div>
-          </div> */}
+          </div> 
 
         </div>
     </div>
