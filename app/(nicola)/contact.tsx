4eeab80ca79b2a0f2a9218d83e8e21fbbd0155c2
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-29 03:35:20 +0600

    asshole done

--- app/(nicola)/contact.tsx
+++ app/(nicola)/contact.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function contact() {
   return (
-     <div className=''>
+     <div className='border contact'>
       
 <form
   action="https://formspree.io/f/mzbldbwv"
   method="POST"
-  className='flex flex-col border-4 items-center my-16 sm:w-1/2 mx-auto pb-5'
+  className='flex flex-col  items-center my-16 sm:w-1/2 mx-auto pb-5'
 >
   <label className=' py-5 flex flex-col'>
     Your email:
@@ -19,7 +19,7 @@ export default function contact() {
   </label>
  
 <div className="btn  w-64 text-start">
-<button type="submit" className='border mt-4 px-4 mb-5 py-2 '>Send</button>
+<button type="submit" className='border-4 font-bold uppercase tracking-wider border-double mt-4 px-4 mb-5 py-2 '>Send</button>
 </div>
 </form>
     </div>
