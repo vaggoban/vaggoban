b113fb42708815fc9cac5629827e80c4ae4ce34e
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-07-31 18:22:50 +0600

    o

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
-import Nav from './(nicola)/nav/page'
-import Health from "./(nicola)/health/page"
-import Care from "./(nicola)/care/page"
-import Support from "./(nicola)/support/page"
-import Contact from './(nicola)/contact'
+
 export default function page() {
   return (
-    <div>
-      <Nav/>
-      <Health/>
-      <Care/>
-      <Support/>
-       <Contact/>
-    </div>
+    <div>page</div>
   )
 }
