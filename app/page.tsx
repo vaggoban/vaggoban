29b405d31ecd6a75726740db2c797f4be52413ba
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-08-04 19:58:21 +0600

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Nav from "./(files)/nav/page"
 import Nav_phone from "./(files)/nav_phone/page"
+import Loading from './loading'
 export default function page() {
   return (
     <div>
-     <Nav_phone/>
+     <Suspense fallback={ <Loading/> } >
+         <Nav_phone/>
+     </Suspense>
      <Nav/>
     </div>
   )
