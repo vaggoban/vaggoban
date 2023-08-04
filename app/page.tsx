c2acb6068238c56553d234d5f775211d9ca83683
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-08-04 20:06:54 +0600

    fuck yourself

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import React, { Suspense } from 'react'
 import Nav from "./(files)/nav/page"
 import Nav_phone from "./(files)/nav_phone/page"
-import Loading from './loading'
+
 export default function page() {
   return (
     <div>
-     <Suspense fallback={ <Loading/> } >
-         <Nav_phone/>
-     </Suspense>
+     <Nav_phone/>
      <Nav/>
     </div>
   )
