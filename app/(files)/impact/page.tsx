8b038d283d7675bd19d8286cf19438b52554c394
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-08-12 13:50:40 +0600

    asshole

--- app/(files)/impact/page.tsx
+++ app/(files)/impact/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import viper from "../img/yo (4).jpg"
-import av from "../img/av.jpg"
+
 export default function page() {
   return (
     <div>
@@ -15,19 +15,15 @@ export default function page() {
                <Image src={viper} alt='viper' className='h-full w-full rounded-full'></Image>
             </div>
             </div>
-           </div>
+           </div> 
            <div className="text px-2 text-teal-400 pb-5  sm:text-slate-600 uppercase text-center font-bold text-2xl tracking-wider pt-5">the red viper</div>
          </div>
         </div>
+
         <div className="two bg-slate-400 sm:w-3/5">
-           khali 
-        <div className="text  w-80 mx-auto">
-        Presentation:
-        </div>
-        <div className="description w-80 mx-auto">
-        Antivenom : Each vial contains lyophilized preparation of Snake Venom Antiserum BP. After reconstitution each ml Snake Venom Antiserum neutralizes not less than Cobra venom (Naja naja) 0.60 mg, Common Krait venom (Bungarus caeruleus) 0.45 mg, Russell's Viper venom (Vipera russelli) 0.60 mg, Saw-scaled Viper venom (Echis carinatus) 0.45 mg.
-        </div>
+           asshole
         </div>  
+
       </div>
     </div>
   )
