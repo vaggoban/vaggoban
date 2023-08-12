58f6ae2e222db60e446aa708bd1cad87c7d5e11c
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-08-12 13:46:35 +0600

    fuck off

--- app/(files)/impact/page.tsx
+++ app/(files)/impact/page.tsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import Image from 'next/image'
 import viper from "../img/yo (4).jpg"
-
+import av from "../img/av.jpg"
 export default function page() {
   return (
     <div>
       <div className="impact sm:flex">
-        <div className="one  sm:w-2/5 bg-slate-400"> <div className="the-color w-48 h-full bg-teal-400 mx-auto"></div>
-         <div className="thesnake">
-         <div className="layer1 p-2 bg-slate-500 rounded-full">
-            <div className="layer2 p-2 bg-teal-400 rounded-full">
+        <div className="one  sm:w-2/5 bg-slate-400">
+           <div className="the-color w-48 h-full bg-teal-400 mx-auto"></div>
+         <div className="thesnake ">
+         <div className="layer1 bg-slate-400 rounded-full mx-auto sm:p-2 ">
+            <div className="layer2 bg-teal-400 rounded-full sm:p-2 ">
             <div className="img h-64 w-64 rounded-full">
                <Image src={viper} alt='viper' className='h-full w-full rounded-full'></Image>
             </div>
             </div>
            </div>
-           <div className="text px-2 text-slate-600 uppercase text-center font-bold text-2xl tracking-wider">the red viper</div>
+           <div className="text px-2 text-teal-400 pb-5  sm:text-slate-600 uppercase text-center font-bold text-2xl tracking-wider pt-5">the red viper</div>
          </div>
         </div>
-        <div className="two border sm:w-3/5">2</div>
+        <div className="two bg-slate-400 sm:w-3/5">
+        <figure className='h-80 w-80 mx-auto'>
+        <Image alt="viper antivenom" src={av} className='h-full w-full rounded-lg'></Image>  
+        </figure>  
+        <div className="text  w-80 mx-auto">
+        Presentation:
+        </div>
+        <div className="description w-80 mx-auto">
+        Antivenom : Each vial contains lyophilized preparation of Snake Venom Antiserum BP. After reconstitution each ml Snake Venom Antiserum neutralizes not less than Cobra venom (Naja naja) 0.60 mg, Common Krait venom (Bungarus caeruleus) 0.45 mg, Russell's Viper venom (Vipera russelli) 0.60 mg, Saw-scaled Viper venom (Echis carinatus) 0.45 mg.
+        </div>
+        </div>  
       </div>
     </div>
   )
