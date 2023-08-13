280d6db4e0f45b6c32c2eaa3ca10d500ed06b43e
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-08-13 16:18:25 +0600

    okk

--- app/(files)/impact/page.tsx
+++ app/(files)/impact/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import viper from "../img/yo (4).jpg"
 import av from "../img/av.jpg"
+import Link from 'next/link'
+
 export default function page() {
   return (
     <div>
@@ -17,13 +19,19 @@ export default function page() {
             </div>
            </div> 
            <div className="text px-2 text-teal-400 pb-5  sm:text-slate-600 uppercase text-center font-bold text-2xl tracking-wider pt-5">the red viper</div>
+           <div className="more  mx-auto text-center text-white">
+            <Link href="/impact">
+              <div className="btn capitalize font-bold border w-24 mx-auto p-2
+              border-slate-400 bg-slate-800 rounded">more info</div>
+              </Link>
+           </div>
          </div>
         </div>
 
         <div className="two bg-slate-400 sm:w-3/5">
            <div className="antivenom">
-            <figure>
-              <Image src={av} alt='ok'></Image>
+            <figure className='w-4/5 border-4 mx-auto' >
+              <Image src={av} alt='ok' className='h-full w-full'></Image>
             </figure> 
            </div>
         </div>  
