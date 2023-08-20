224fc054750f86efe32e4549b464068d478abbbc
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-08-20 14:21:11 +0600

    again yo lets finish it

--- app/(files)/impact/page.tsx
+++ app/(files)/impact/page.tsx
@@ -35,8 +35,8 @@ export default function page() {
             </figure> 
            </div>
            <div className="text">
-            <div className="lone1 text-2xl w-4/5 mx-auto text-white capitalize font-bold">the red viper :</div>
-            <div className="line2 w-4/5 mx-auto text-white capitalize pb-5"> Viperidae are a family of snakes found in most parts of the world, except for Antarctica, Australia, Hawaii, Madagascar, New Zealand, and various other isolated islands. They are venomous and have long, hinged fangs that permit deep penetration and injection of their venom.</div>
+            <div className="lone1 text-2xl w-4/5 mx-auto  capitalize font-bold">the red viper :</div>
+            <div className="line2 w-4/5 mx-auto capitalize pb-5"> Viperidae are a family of snakes found in most parts of the world, except for Antarctica, Australia, Hawaii, Madagascar, New Zealand, and various other isolated islands. They are venomous and have long, hinged fangs that permit deep penetration and injection of their venom.</div>
            </div>
         </div>  
 
--- app/(files)/support/page.tsx
+++ app/(files)/support/page.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-     <div className="support">
-      <div className="head">the antivenom support section</div>
+    <div className='bg-slate-300'>
+     <div className="support py-12 ">
+      <div className="head uppercase text-4xl text-center font-bold tracking-wider">the antivenom support section</div>
+      <div className="text text-center capitalize mt-2">we provide the best antivenom service in the whole country</div>
+      <div className="text text-center capitalize px-3 py-2 rounded bg-teal-400 w-80 mt-3 mx-auto"><a href="https://my.clevelandclinic.org/health/diseases">click here fof a premium service</a></div>
      </div>
     </div>
   )
