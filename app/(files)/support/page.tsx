897d4b29b4a162cb4072f907dde5b4464366ad8b
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-08-20 14:33:06 +0600

    was

--- app/(files)/support/page.tsx
+++ app/(files)/support/page.tsx
@@ -8,6 +8,25 @@ export default function page() {
       <div className="text text-center capitalize mt-2">we provide the best antivenom service in the whole country</div>
       <div className="text text-center capitalize px-3 py-2 rounded bg-teal-400 w-80 mt-3 mx-auto"><a href="https://my.clevelandclinic.org/health/diseases">click here fof a premium service</a></div>
      </div>
+
+     
+<form
+  action="https://formspree.io/f/mzbldbwv"
+  method="POST" className='pb-5 flex flex-col gap-4 '
+>
+  <label className=' flex flex-col gap-2 w-64 mx-auto'>
+    Your email:
+    <input type="email" name="email" className='w-64 h-8 rounded' />
+  </label>
+
+  <label className=' flex flex-col gap-2 w-64 mx-auto'>
+    Your message:
+    <textarea name="message" className='w-64 h-48 rounded'></textarea>
+  </label>
+ 
+  <button type="submit" className=' w-20 mx-auto py-2 rounded bg-teal-400 tracking-wider uppercase'>Send</button>
+</form>
+
     </div>
   )
 }
