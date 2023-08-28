bfdbc92b98d6d9c78028dd9e9146719016a7c907
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-08-28 18:16:35 +0600

    deikkha loi age

--- app/(nicola)/Home/page.tsx
+++ app/(nicola)/Home/page.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
+import smoke from "../img/l (1).jpg"
+
 
 const page = () => {
   return (
     <div>
-        home
+        <div className="home">
+          <div className="text text-4xl uppercase tracking-wider sm:w-3/4  mx-auto color pl-8 py-2">home</div>
+          <figure className='sm:w-3/4 px-8 mx-auto'>
+            <Image alt='smoke' src={smoke} className='w-full rounded' ></Image>
+          </figure>
+          <div className="text capitalize text-2xl mx-auto px-8 text-center sm:w-3/4 mt-5">
+            lucky strike : The Best Choice for Cheap and Convenient Shopping
+          </div>
+          <div className="text text-center px-8 py-8">
+          We recognize that smoking can be a costly habit, and we want to help our clients save money without losing the quality of their smoking experience at LuckyStrikCigs.com. Continue reading to learn about the advantages of purchasing Lucky Strike cigarettes from our website. We can make the delivery procedure simple and convenient for our consumers.
+          </div>
+          <div className="text-2xl text-center">
+          Lucky Strike Cigarettes Are a Perfect Choice for Smokers.
+          </div>
+        </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Nav from "./(nicola)/nav"
 import Thelogo from './(nicola)/thelogo'
+import Home from "./(nicola)/Home/page"
+
+
+
 const page = () => {
   return (
     <div>
       <Thelogo/>
       <Nav/>
+      <Home/>
+
     </div>
   )
 }
