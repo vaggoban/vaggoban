04fe341406f5fa1d2de1d1b9ebf32ee255fecea8
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-08-31 18:13:55 +0600

    done

--- app/(nicola)/Shop/page.tsx
+++ app/(nicola)/Shop/page.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+
 const page = () => {
   return (
-    <div>shop</div>
+    <div>
+      <div className="shop sm:grid sm:grid-cols-2 items-center bg-gray-200">
+        <div className="left border py-12">
+          <div className="img_cigarette h-96 w-80 mx-auto"></div>
+        </div>
+        <div className="right">
+          <div className="text-4xl text-center px-3 py-2 sm:text-start">
+          Fast and Convenient International Shipping for Your Smoking Needs
+          </div>
+          <div className="text-center px-4 sm:text-start">
+          For us, the necessity of speedy and simple shipment comes first. That is why we provide a range of delivery alternatives to ensure that your cigarettes arrive quickly. We have you covered whether you want standard or expedited shipment. Our shipping fees are among the most reasonable on the internet, making it simple for you to obtain your favorite smokes without spending a bunch. We will gladly mail the items to our consumers regardless of where they are, hence we provide international delivery. We mail our smokes to over 200 countries worldwide, so you may enjoy your favorite Lucky Strikes no matter where you are.
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Nav from "./(nicola)/nav"
 import Thelogo from './(nicola)/thelogo'
 import Home from "./(nicola)/Home/page"
-
+import Shop from "./(nicola)/Shop/page"
 
 
 const page = () => {
@@ -11,7 +11,7 @@ const page = () => {
       <Thelogo/>
       <Nav/>
       <Home/>
-
+      <Shop/>
     </div>
   )
 }
