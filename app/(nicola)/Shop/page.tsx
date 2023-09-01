fa3580d62f856098d118cd8b914cb7b4f7d60ab2
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-01 18:05:14 +0600

    hi

--- app/(nicola)/Shop/page.tsx
+++ app/(nicola)/Shop/page.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      hey ive a better plan
+      <div className="the_shop sm:flex sm:items-center">
+        <div className="left">left</div>
+        <div className="right">right</div>
+      </div>
     </div>
   )
 }
