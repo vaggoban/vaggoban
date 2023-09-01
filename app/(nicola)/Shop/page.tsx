185297deba74c06d3bfdfc5353d37d6cc52e3917
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-01 17:54:55 +0600

    lemme see now

--- app/(nicola)/Shop/page.tsx
+++ app/(nicola)/Shop/page.tsx
@@ -5,9 +5,19 @@ import black from "../img/B1.jpg"
 
 const page = () => {
   return (
-    <div>
-     is that okay now
-    </div>
+        <div className="shop">
+          <div className="left">
+           <Image alt='smoke up yo' src={black} className='h-96 w-80'></Image>
+          </div>
+          <div className="right">
+          <div className="text-4xl text-center px-3 py-2 sm:text-start">
+          Fast and Convenient International Shipping for Your Smoking Needs
+          </div>
+          <div className="text-center px-4 sm:text-start">
+          For us, the necessity of speedy and simple shipment comes first. That is why we provide a range of delivery alternatives to ensure that your cigarettes arrive quickly. We have you covered whether you want standard or expedited shipment. Our shipping fees are among the most reasonable on the internet, making it simple for you to obtain your favorite smokes without spending a bunch. We will gladly mail the items to our consumers regardless of where they are, hence we provide international delivery. We mail our smokes to over 200 countries worldwide, so you may enjoy your favorite Lucky Strikes no matter where you are.
+          </div>
+        </div>
+        </div>
   )
 }
 
