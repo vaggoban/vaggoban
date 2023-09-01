56b15c73eebf806cb52d62a8c4e6c3034b7048fc
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-01 17:57:08 +0600

    whatever

--- app/(nicola)/Shop/page.tsx
+++ app/(nicola)/Shop/page.tsx
@@ -6,11 +6,11 @@ import black from "../img/B1.jpg"
 const page = () => {
   return (
         <div className="shop">
-          <div className="left">
+          <div className="one">
            <Image alt='smoke up yo' src={black} className='h-96 w-80'></Image>
           </div>
-          <div className="right">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum distinctio aspernatur quod in dignissimos totam, possimus consequatur quae voluptates earum, recusandae minima. Neque consequuntur facere adipisci mollitia necessitatibus eum nemo repellendus corporis reprehenderit?
+          <div className="two">
+            whatever
           </div>
         </div>
   )
