830efc64c620a6fb1731e86f83e6c9a359072c64
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-01 19:12:10 +0600

    ne guzel bu

--- app/(nicola)/Shop/page.tsx
+++ app/(nicola)/Shop/page.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
 
-
-
-
 export default function page() {
   return (
-    <div>
-      <div className="the_shop sm:flex sm:items-center">
-        <div className="left ">
-          <div className="img "></div>
-        </div>
-        <div className="right">right</div>
-      </div>
-    </div>
+    <div>page</div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import Nav from "./(nicola)/nav"
 import Thelogo from './(nicola)/thelogo'
 import Home from "./(nicola)/Home/page"
-import Shop from "./(nicola)/Shop/page"
-
+import Order from "./(nicola)/Order_Track/page"
 
 const page = () => {
   return (
@@ -11,7 +10,7 @@ const page = () => {
       <Thelogo/>
       <Nav/>
       <Home/>
-      <Shop/>
+      <Order/>
     </div>
   )
 }
