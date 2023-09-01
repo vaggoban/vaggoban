7fe3e99c5ffdf4567b47d9ae76b1600a484fc2df
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-01 17:58:11 +0600

    hey the better plan

--- app/(nicola)/Shop/page.tsx
+++ app/(nicola)/Shop/page.tsx
@@ -1,19 +1,9 @@
 import React from 'react'
-import Image from 'next/image'
-import black from "../img/B1.jpg"
 
-
-const page = () => {
+export default function page() {
   return (
-        <div className="shop">
-          <div className="one">
-           <Image alt='smoke up yo' src={black} className='h-96 w-80'></Image>
-          </div>
-          <div className="two">
-            whatever
-          </div>
-        </div>
+    <div>
+      hey ive a better plan
+    </div>
   )
 }
-
-export default page
