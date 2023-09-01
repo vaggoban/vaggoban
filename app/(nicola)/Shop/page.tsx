cf6a1764f82c71da4f6a66e12bcb0644c74f6301
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-01 18:09:42 +0600

    zara

--- app/(nicola)/Shop/page.tsx
+++ app/(nicola)/Shop/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
+import kalada from "../img/B1.jpg"
+
 
 export default function page() {
   return (
     <div>
       <div className="the_shop sm:flex sm:items-center">
-        <div className="left">left</div>
+        <div className="left w-80 h-96">
+          <Image alt='whatever' src={kalada} className='h-full w-full'></Image>
+        </div>
         <div className="right">right</div>
       </div>
     </div>
