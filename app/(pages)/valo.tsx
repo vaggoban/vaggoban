a9dbe8273330de3ea1d0813fa5d9d2998ade9de9
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-04 18:54:06 +0600

    ami hoisi arrek pod

--- app/(pages)/valo.tsx
+++ app/(pages)/valo.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function valo() {
   return (
-    <div>valo hoye jaw yo </div>
+    <div>
+      valo hoye jaw bitcH dekha jak
+      </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import Nav from "./(nicola)/nav"
-import Thelogo from './(nicola)/thelogo'
-import Home from "./(nicola)/Home/page"
-import Order from "./(nicola)/Order_Track/page"
-
-const page = () => {
+import Valo from './(pages)/valo'
+export default function page() {
   return (
     <div>
-      <Thelogo/>
-      <Nav/>
-      <Home/>
-      <Order/>
+      <Valo/>
     </div>
   )
 }
-
-export default page
