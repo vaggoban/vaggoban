2790fca39aa6c46e6d7a9ccd5893b21e4561ca2a
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-09 13:00:44 +0600

    v.1.00

--- app/(pages)/nav_sm.tsx
+++ app/(pages)/nav_sm.tsx
@@ -19,7 +19,7 @@ const nav_sm = () => {
           <input type="checkbox" className='hidden' />
     
 
-          <div className="links hidden w-5/6 capitalize text-center py-12">
+          <div className="links hidden w-5/6 capitalize text-center py-12 text-sky-600">
             <Link href="./shop"><div className="shop px-3 py-2 border w-28
             mx-auto bg-gray-200 rounded ">shop</div></Link>
             <Link href="./order_track"><div className="shop px-3 py-2 border w-28 mx-auto my-12  bg-gray-200 rounded ">order track</div></Link>
