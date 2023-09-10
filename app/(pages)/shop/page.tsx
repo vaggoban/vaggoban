1c67b7cf5b8d603ee254c7ff2cc623e9171f7191
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-10 16:51:14 +0600

    who

--- app/(pages)/shop/page.tsx
+++ app/(pages)/shop/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
+
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <div className="shop py-3 sm:px-12 border ">
+        <div className="text capitalize font-bold tracking-wider w-5/6 sm:w-3/4 mx-auto py-2 ">cool crunch ..</div>
+         <div className=" w-5/6 sm:w-3/4 h-96 mx-auto ">
+            <div className="image w-full h-full rounded">
+              
+            </div>
+         </div>
+      </div>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Logo from './(pages)/logo'
 import Nav from "./(pages)/nav"
 import Navsm from "./(pages)/nav_sm"
+import Shop from "./(pages)/shop/page"
 
 
 
@@ -11,6 +12,7 @@ export default function page() {
        <Logo/>
        <Nav/>
        <Navsm/>
+       <Shop/>
     </div>
   )
 }
