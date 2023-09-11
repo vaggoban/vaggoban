68c7582857480328414ecb5cca4ab4fdec4e7aa3
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-11 07:22:52 +0600

    lv.7.22

--- app/(pages)/order_track/page.tsx
+++ app/(pages)/order_track/page.tsx
@@ -32,6 +32,7 @@ const page = () => {
             </div>
             <div className="btn border border-sky-600 capitalize px-3 py-2 rounded w-fit mx-auto my-2">add to cart</div>
           </div>
+
   <div className="two w-full sm:w-fit mx-auto">
   <figure className='h-96 w-64 py-5 mx-auto'>
     <Image alt='smoke2' className='h-full w-full rounded' src={two}></Image>
@@ -52,6 +53,27 @@ const page = () => {
   <div className="btn border border-sky-600 capitalize px-3 py-2 rounded w-fit mx-auto my-2">add to cart</div>
 </div>
 
+
+       
+<div className="three  w-full sm:w-fit mx-auto">
+  <figure className='h-96 w-64 py-5 mx-auto'>
+    <Image alt='smoke' className='h-full w-full rounded' src={three}></Image>
+  </figure>
+  <div className="text-center mb-2 text-xl">Lucky Strike wild Box</div>
+  <div className="text-center">
+  <button className='uppercase border border-sky-600 px-3 py-1.5 rounded tracking-wider text-center m-2'>sale</button>
+  </div>
+  <div className="price text-center">
+   <div className="old_new_price flex justify-center gap-3">
+   <div className="dollar">
+      <span className=''>$56.99</span>
+      <div className="line"></div>
+    </div>
+    <div className="new_dollar">$43.99</div>
+   </div>
+  </div>
+  <div className="btn border border-sky-600 capitalize px-3 py-2 rounded w-fit mx-auto my-2">add to cart</div>
+</div>
          
         </div>
       </div>
