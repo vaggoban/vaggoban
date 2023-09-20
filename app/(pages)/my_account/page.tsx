16bc5b89936ea5d9990a6147b8731ea5549bbe4d
mrg25mrg25 <mrg25mrg25@gmail.com>
2023-09-20 17:49:27 +0600

    no not at all

--- app/(pages)/my_account/page.tsx
+++ app/(pages)/my_account/page.tsx
@@ -3,15 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <div className="myaccount p-4  ">
-        <div className="text-center text-4xl font-bold">Satisfaction Guaranteed</div>
-        <div className="text-center text-lg font-bold tracking-wider pt-5 sm:pt-2">
-        We cherish our clients at LuckyStrikCigs.com, and their pleasure is our main concern. Our goal is for our clients to be entirely delighted with their purchase. If you are not totally pleased with your purchase for any reason, we will do everything we can to make it right.
-        </div>
-        <div className="text-center pt-5">
-        We guarantee our customers an outstanding experience from the moment they start browsing our selection to the moment they receive their purchase. With authentic products and reasonable prices, we believe we can provide the best service possible. Our satisfied customers are a testament to our commitment to excellence. Don’t take our word for it – try us out and see for yourself why so many smokers trust us for their tobacco needs. When you order from us, you can rest assured that you will receive a high-quality product delivered straight to your door, with free worldwide shipping options available
-        </div>
-      </div>
+      whu
    
     </div>
   )
